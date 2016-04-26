6e48ab1c3c74f3d009c243b35a53af9310ef6ef7
Keith <keithbradley1@msn.com>
2016-04-26 15:55:55 -0500

    created the translateTO files

---
+++ translateToJapanese.js
@@ -0,0 +1,9 @@
+translate = (function(){
+  var JapaneseLexicon = {
+    "happy": "happi",
+  };
+  translate.getJapaneseWord = function(){
+    return japaneseLexicon;
+  };
+  return word;
+})(translate);

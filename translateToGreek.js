7f324b56acddc453812ca4c969674d9f19fdc6ec
Keith <keithbradley1@msn.com>
2016-04-27 11:32:50 -0500

    changed type to fix merge conflict

--- translateToGreek.js
+++ translateToGreek.js
@@ -1,6 +1,6 @@
 translate = (function(word){
   var greekLexicon = {
-    "happy ": "Χαρούμενα", "birthday": "Γενέθλια", "now": "τώρα", "you are":"είστε", "old":"παλιός", "mothers": "Μητέρα", "day":
+    "happy": "Χαρούμενα", "birthday": "Γενέθλια", "now": "τώρα", "you are":"είστε", "old":"παλιός", "mothers": "Μητέρα", "day":
 "Hμέρα", "to": "να", "the": "ο", "best": "καλύτερος", "dog": "μαμά", "in": "σε", "world": "κόσμος", "valentines": "Βαλεντίνος", "be": "είναι", "mine": "ορυχείο", "and": "και", "chill": "ψύχρα"};
   translate.getGreekWord = function(){
     return greekLexicon;
